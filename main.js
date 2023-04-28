dda50369b81c478d3eb20da2df7941f53ee2b12b
Manuel-25 <manuelotamendi97@gmail.com>
2023-04-28 17:17:33 -0300

    file system terminado con manejo de errores

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+import fs from 'fs'
 
 class ProductManager {
     constructor (path) {
@@ -23,6 +23,10 @@ class ProductManager {
         }
     }
 
+    async writeFile(data_json) {
+        await fs.promises.writeFile(this.path, data_json)
+    }
+
     async add_Product({ title, description, price, thumbnail, code, stock }) {
         try {
             const existingProduct = this.products.find(product => product.code === code)
@@ -35,7 +39,7 @@ class ProductManager {
             this.products.length === 0 ? data.id = 1 : data.id = this.products[this.products.length-1].id + 1
             this.products.push(data)
             let data_json = JSON.stringify(this.products, null, 2)
-            await fs.promises.writeFile(this.path, data_json)
+            this.writeFile(data_json)
             return data.id
         } catch(err) {
             return 'addProduct: error', err
@@ -57,33 +61,35 @@ class ProductManager {
     getProductById(id) {
         let product = this.products.find(product => product.id === id)
         if(!product) {
-            return 'getProductById: error'
+            return null
         }
         return product
     }
 
     async updateProduct(id, data) {
         try {
-            let product = this.getProductById(id)
             //Verificacion de datos y errores
+            let product = this.getProductById(id)
             if(!product) {
-                return 'updateProduct: error user not found'
+                return 'updateProduct error: user not found'
             }
 
             if(Object.keys(data).length === 0) {
-                return 'updateProduct: error insert some values'
+                return 'updateProduct error: insert some values'
             }
 
             for (let prop in data) {
-                /*if(prop != 'title' || prop != 'description' || prop != 'price' || prop != 'thumbnail' || prop != 'code' || prop != 'stock') {
+                /*
+                    por alguna razon no funciona
+                if(prop != 'title' || prop != 'description' || prop != 'price' || prop != 'thumbnail' || prop != 'code' || prop != 'stock') {
                     console.log(`updateProduct: error ${prop} is not a property of product`)
                 }*/
 
                 product[prop] = data[prop]
             }
             let data_json = JSON.stringify(this.products, null, 2)
-            await fs.promises.writeFile(this.path, data_json)
-            return 'updateProduct: done'
+            this.writeFile(data_json)
+            return 'updateProduct: ' + id + ' done' 
         } catch(err) {
             return 'updateProduct: error', err
         }
@@ -91,11 +97,13 @@ class ProductManager {
 
     async deleteProduct(id) {
         try {
+            if(!this.getProductById(id)) {
+                return 'deleteProduct error: product not found'
+            }
             this.products = this.products.filter(each => each.id !== id)
-            console.log(this.products)
             let data_json = JSON.stringify(this.products, null, 2)
-            await fs.promises.writeFile(this.path, data_json)
-            return 'deleteProduct: done'
+            this.writeFile(data_json)
+            return 'deleteProduct: ' + id + ' done'
         } catch(err) {
             return 'deleteProduct: error', err
         }
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const ProductManager = require('./main.js')
 
+let producto = new ProductManager("./data/data.json")
 
 async function manager() {
-    let producto = new ProductManager("./data/data.json")
     await producto.add_Product({ title: 'producto 1', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code: 'abc123'})
     await producto.add_Product({ title: 'producto 2', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code: 'abc124'})
     await producto.add_Product({ title: 'producto 3', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code: 'abc125'})
@@ -15,8 +15,8 @@ async function manager() {
     await producto.add_Product({ title: 'producto 10', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code: 'abc132'})
     
     //await producto.getProductById(9)
-    await producto.updateProduct(9, { title: 'producto 11', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code: 'abc131'})
-    //await producto.deleteProduct(10)
+    //await producto.updateProduct(9, { title: 'producto 11', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code: 'abc131'})
+    console.log(await producto.deleteProduct(10))
     
     //console.log(await producto.getProducts())
 }
