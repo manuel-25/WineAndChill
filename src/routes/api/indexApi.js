93104df028ed76b516bf9ef31b3a8f409ffcb5a1
manuel-25 <manuelotamendi97@gmail.com>
2023-07-20 19:02:08 -0300

    Removed express session

--- src/routes/api/indexApi.js
+++ src/routes/api/indexApi.js
@@ -2,7 +2,6 @@ import { Router } from "express"
 import products_router from './productRoutes.js'
 import cart_router from './cartRoutes.js'
 import cookies_router from './cookies.js'
-import session_router from './sessions.js'
 import auth_router from './authRoutes.js'
 
 const router = Router()
@@ -10,7 +9,6 @@ const router = Router()
 router.use('/products', products_router)
 router.use('/carts', cart_router)
 router.use('/cookies', cookies_router)
-router.use('/session', session_router)
 router.use('/auth', auth_router)
 
 
