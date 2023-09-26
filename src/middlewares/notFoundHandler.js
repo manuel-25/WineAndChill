5d5c8d86655a513cf61b71e75d152a5edf76f166
Manuel-25 <manuelotamendi97@gmail.com>
2023-09-26 16:48:23 -0300

    notFoundHandler.js now renders error view

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -2,14 +2,15 @@ import { logger } from "../config/logger.js";
 
 const notFoundHandler = (req, res, next) => {
     const errorMessage = `Route not found: ${req.method} ${req.url}`
-    logger.error(errorMessage)
-
-    return res.status(404).json({
-        status: 404,
-        method: req.method,
-        path: req.url,
-        message: 'Route not found',
-        error: errorMessage,
+    logger.debug(errorMessage)
+    return res.render('error', {
+        title: 'Error',
+        style: 'error.css',
+        script: '',
+        error: {
+            status: 404,
+            message: errorMessage
+        }
     })
 }
 
