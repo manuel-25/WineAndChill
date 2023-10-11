4392ca5994504b8a8af2346a46e3706e899f365f
Manuel-25 <manuelotamendi97@gmail.com>
2023-10-11 18:20:56 -0300

    Product deleted sendEmail done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,163 +1,187 @@
 import { productService, userService } from "../Service/index.js"
+import config from "../config/config.js"
+import { sendProductDeletedEmail } from "../utils/sendEmail.js"
 
 class ProductController {
   async getProducts(req, res, next) {
     try {
-      const limit = !isNaN(parseInt(req.query.limit)) ? parseInt(req.query.limit) : 6;
-      const page = !isNaN(parseInt(req.query.page)) ? parseInt(req.query.page) : 1;
-      const searchTerm = req.query.title || '';
+      const limit = !isNaN(parseInt(req.query.limit)) ? parseInt(req.query.limit) : 6
+      const page = !isNaN(parseInt(req.query.page)) ? parseInt(req.query.page) : 1
+      const searchTerm = req.query.title || ''
   
-      // Modificar para dividir en palabras clave correctamente
-      const keywords = searchTerm.trim().split(/\s+/);
+      // Modifica para dividir en palabras clave correctamente
+      const keywords = searchTerm.trim().split(/\s+/)
   
-      let query = {};
+      let query = {}
   
       if (keywords.length > 0) {
-        // Construir consulta con filtros de título y categoría si hay palabras clave
+        // Construye consulta con filtros de título y categoria si hay palabras clave
         query.$or = keywords.map(keyword => ({
           $or: [
             { title: new RegExp(keyword, 'i') },
             { type: new RegExp(keyword, 'i') },
             { cellar: new RegExp(keyword, 'i') }
           ]
-        }));
+        }))
       }
   
-      const all = await productService.paginate(query, { limit, page, lean: true });
+      const all = await productService.paginate(query, { limit, page, lean: true })
   
       if (!all || all.error) {
         return res.status(404).send({
           status: 404,
           response: all.error || 'Products pagination error!'
-        });
+        })
       }
   
       return res.status(200).send({
         status: 200,
         response: all
-      });
+      })
     } catch (error) {
-      next(error);
+      next(error)
     }
   }
   
+  async getProduct(req, res, next) {
+    try {
+      const productId = req.params.pid
+      const product = await productService.getById(productId)
+
+      if (!product) {
+        return res.status(404).send({
+          status: 404,
+          response: `Failed to get product ${productId}`
+        })
+      }
+      return res.status(200).send({
+        status: 200,
+        response: product
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
   
-    async getProduct(req, res, next) {
-      try {
-        const productId = req.params.pid
-        const product = await productService.getById(productId)
-  
-        if (!product) {
-          return res.status(404).send({
+  async createProduct(req, res, next) {
+    try {
+      const token = req.token ?? null
+      req.body.owner = token.email
+
+      const response = await productService.createProduct(req.body)
+      if (!response) {
+          return res.status(404).json({
             status: 404,
-            response: `Failed to get product ${productId}`
+            response: 'Failed to create product!'
+          })
+      }
+      return res.status(201).json({
+        status: 201,
+        response: `Product ${response._id} created!`,
+        payload: response
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+  
+  async updateProduct(req, res, next) {
+    try {
+      const { pid } = req.params
+      const data = req.body
+      const userEmail = req.token.email
+
+      if (!pid || !data) {
+        return res.status(500).json({
+          status: 500,
+          response: 'Update Error: Please provide a valid product ID or data.',
+        })
+      }
+
+      const userData = await userService.getByEmail(userEmail)
+      const productData = await productService.getById(pid)
+
+      if(productData.owner === userData.email || userData.role === 'OWNER') {
+        const product = await productService.updateProduct(pid, data, { new: true })
+        if (!product) {
+          return res.status(404).json({
+            status: 404,
+            response: `Update Error: Product ${pid} not updated!`
           })
         }
-        return res.status(200).send({
-          status: 200,
-          response: product
-        })
-      } catch (error) {
-        next(error)
-      }
-    }
-  
-    async createProduct(req, res, next) {
-      try {
-        const token = req.token ?? null
-        req.body.owner = token.email
 
-        const response = await productService.createProduct(req.body)
-        if (!response) {
-            return res.status(404).json({
-              status: 404,
-              response: 'Failed to create product!'
-            })
-        }
         return res.status(201).json({
           status: 201,
-          response: `Product ${response._id} created!`,
-          payload: response
+          response: `Product ${pid} updated!`,
+          productId: pid,
+          payload: product
         })
-      } catch (error) {
-        next(error)
+      } else {
+        return res.status(401).json({
+          status: 401,
+          response: `Update Error: Not authorized! Owner: ${productData.owner}`
+        })
       }
-    }
-  
-    async updateProduct(req, res, next) {
-      try {
-        const { pid } = req.params
-        const data = req.body
-        const userEmail = req.token.email
-  
-        if (!pid || !data) {
-          return res.status(500).json({
-            status: 500,
-            response: 'Update Error: Please provide a valid product ID or data.',
-          })
-        }
-
-        const userData = await userService.getByEmail(userEmail)
-        const productData = await productService.getById(pid)
-
-        if(productData.owner === userData.email || userData.role === 'OWNER') {
-          const product = await productService.updateProduct(pid, data, { new: true })
-          if (!product) {
-            return res.status(404).json({
-              status: 404,
-              response: `Update Error: Product ${pid} not updated!`
-            })
-          }
-
-          return res.status(201).json({
-            status: 201,
-            response: `Product ${pid} updated!`,
-            productId: pid,
-            payload: product
-          })
-        } else {
-          return res.status(401).json({
-            status: 401,
-            response: `Update Error: Not authorized! Owner: ${productData.owner}`
-          })
-        }
-      } catch (error) {
-        next(error)
-      }
-    }
-  
-    async deleteProduct(req, res, next) {
-      try {
-        const pid = req.params.pid
-        const userEmail = req.token.email
-
-        const userData = await userService.getByEmail(userEmail)
-        const productData = await productService.getById(pid)
-
-        if(productData.owner === userData.email || userData.role === 'OWNER') {
-          const product = await productService.deleteProduct(pid)
-          if (!product) {
-            return res.status(404).json({
-              status: 404,
-              response: `Delete Error: Product ${pid} not deleted!`
-            })
-          }
-          return res.status(200).json({
-            status: 200,
-            response: `Product ${pid} deleted!`,
-            payload: product
-          })
-        } else {
-          return res.status(401).json({
-            status: 401,
-            response: `Delete Error: Not authorized! Owner: ${productData.owner}`
-          })
-        }
-      } catch (error) {
-        next(error)
-      }
+    } catch (error) {
+      next(error)
     }
   }
+  
+  async deleteProduct(req, res, next) {
+    try {
+      const pid = req.params.pid
+      const userEmail = req.token.email
+      if(!pid || !userEmail) {
+        return res.status(400).send({
+          success: false,
+          message: 'Product Id or Token invalid'
+        })
+      }
+
+      const userData = await userService.getByEmail(userEmail)
+      if(!userData) {
+        return res.status(400).send({
+          success: false,
+          message: 'Token email invalid'
+        })
+      }
+      const product = await productService.getById(pid)
+      if(!product) {
+        return res.status(400).send({
+          success: false,
+          message: 'Invalid product'
+        })
+      }
+
+      if(product.owner === userData.email || userData.role === 'OWNER') {
+        const productDeleted = await productService.deleteProduct(pid)
+        if (!productDeleted) {
+          return res.status(404).json({
+            status: 404,
+            response: `Delete Error: Product ${pid} not deleted!`
+          })
+        }
+        if(product.owner !== config.ADMIN_EMAIL) {
+          console.log('product.owner:', product.owner)
+          const ownerData = await userService.getByEmail(product.owner)
+          sendProductDeletedEmail(ownerData, product)
+        }
+        return res.status(200).json({
+          status: 200,
+          response: `Product ${pid} deleted!`,
+          payload: productDeleted
+        })
+      } else {
+        return res.status(401).json({
+          status: 401,
+          response: `Delete Error: Not authorized! Owner: ${product.owner}`
+        })
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+}
   
 export default new ProductController()
   
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -106,13 +106,70 @@ export const sendTicketEmail = async (userData, ticket) => {
                     </div>
                 </body>
                 </html>
+            
             `
-        });
+        })
     } catch (err) {
         logger.error(`Error al enviar el correo electrónico a ${userData.email} sobre resumen de compra`, err)
     }
 }
 
-  
-  
-  
+export const sendProductDeletedEmail = async (userData, product) => {
+    try {
+        const { email, name } = userData;
+
+        await transport.sendMail({
+            from: `<${config.GMAIL_USER}>`,
+            to: `<${email}>`,
+            subject: "Producto Eliminado",
+            html: `
+                <html>
+                <head>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                        }
+                        .container {
+                            max-width: 600px;
+                            margin: 0 auto;
+                            padding: 20px;
+                        }
+                        .header {
+                            background-color: #f2f2f2;
+                            padding: 10px;
+                            text-align: center;
+                        }
+                        .content {
+                            padding: 20px;
+                        }
+                        .footer {
+                            background-color: #f2f2f2;
+                            padding: 10px;
+                            text-align: center;
+                        }
+                        .violet {
+                            color: #8a56ac;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class="container">
+                        <div class="header">
+                            <h1>Hola ${name},</h1>
+                            <p>Lamentamos informarte que tu producto "${product.title}" Id: ${product._id} ha sido eliminado.</p>
+                        </div>
+                        <div class="content">
+                            <p>Si tienes alguna pregunta o necesitas más información, no dudes en contactarnos.</p>
+                        </div>
+                        <div class="footer">
+                            <p class="violet">Wine and Chill</p>
+                        </div>
+                    </div>
+                </body>
+                </html>
+            `
+        });
+    } catch (err) {
+        logger.error(`Error al enviar el correo electrónico a ${userData.email} sobre la eliminación del producto`, err);
+    }
+}
