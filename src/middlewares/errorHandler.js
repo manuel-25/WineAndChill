abb92fbaf6e8a1b4b30552edcebf3d576300e9e1
manuel-25 <manuelotamendi97@gmail.com>
2023-10-19 22:06:07 -0300

    error handler update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,6 +3,9 @@ import { logger } from "../config/logger.js"
 const errorHandler = (err, req, res, next) => {
     console.error(err)
     logger.error(`status: ${err.status || 500} method: ${req.method} path: ${req.url} message: ${err.message}`)
+    if (err.stack) {
+        logger.error(`Error stack trace: ${err.stack}`);
+    }
     return res.render('error', {
         title: 'Error',
         style: 'error.css',
